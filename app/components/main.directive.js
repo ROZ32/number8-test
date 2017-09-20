7e4f6446f93eb69ddce3c13ce2090350be864de1
Oscar Sanchez <oscar.sanchez@laureate.net>
2017-09-19 22:43:59 -0600

    get remaing months

--- app/components/main.directive.js
+++ app/components/main.directive.js
@@ -11,8 +11,43 @@ app.directive('main', function() {
 		controllerAs: 'main',
 		controller: function() {
 			var calendarFactory = new calendar.Calendar();
+
+			var getRemaingMonths = () => {
+				var counter = 0;
+				var initMonth = this.date.getMonth();
+				var initYear = this.date.getFullYear();
+				angular.forEach(this.initMonth, (week) => {
+					angular.forEach(week, (day) => {
+						if (day >= this.date.getDate()) {
+							counter++;
+						}
+					});
+				});
+				
+				while(counter != this.number) {
+					this.remaingMonths = [];
+					if (initMonth == 11) {
+						initMonth = 0;
+						initYear++;
+					} else {
+						initMonth++;
+					}
+
+					var nextMonth = calendarFactory.monthDays(initMonth, initYear);
+					this.remaingMonths.append(nextMonth);
+					angular.forEach(nextMonth, (week) => {
+						angular.forEach(week, (day) => {
+							if (day >= this.date.getDate()) {
+								counter++;
+							}
+						});
+					});
+				}
+			};
+
 			this.renderCalendar = () => {
 				this.initMonth = calendarFactory.monthDays(this.date.getFullYear(), this.date.getMonth());
+				getRemaingMonths();
 			};
 		}
 	};
